67b9f036148ba84ac9bf4dcf8158b74f1e146eba
Vitrus <67698717+vitrusys@users.noreply.github.com>
2023-08-20 15:40:29 +0200

    Bingo buttons added, still needs some optimisation.

--- src/commands/tools/generatePromptList.js
+++ src/commands/tools/generatePromptList.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const {SlashCommandBuilder} = require('discord.js');
 const {shuffleArray, getJson} = require("../../misc/dataManager");
 
 module.exports = {
@@ -8,7 +8,7 @@ module.exports = {
         .addStringOption(option =>
             option
                 .setName('type')
-                .setDescription('Type of Sheet that should be generated')
+                .setDescription('Type of the Prompts that should be generated')
                 .setRequired(true)
                 .setChoices(
                     {name: 'PC', value: 'pcOnly'},
@@ -16,22 +16,23 @@ module.exports = {
                     {name: 'Poicast', value: 'poicast'},
                     {name: 'General', value: 'general'},
                     {name: 'Gameday', value: 'gamedayOnly'},
-                    )),
+                )),
 
     async execute(interaction) {
         const type = interaction.options.getString('type')
 
         let data = getJson('res/general.json')
+        //TODO optimize
         let newData = data
         if (type !== 'general') {
-            const tempData = getJson('res/'+type+'.json')
+            const tempData = getJson('res/' + type + '.json')
             newData = data.concat(tempData)
         }
         if (type === 'poicast') {
-            newData = await getJson('res/'+type+'.json')
+            newData = await getJson('res/' + type + '.json')
         }
 
-        const shuffledData= shuffleArray(newData);
+        const shuffledData = shuffleArray(newData);
         const output = shuffledData.slice(0, 25);
         const dataArray = Array.from(output);
 
--- src/commands/tools/makeBingo.js
+++ src/commands/tools/makeBingo.js
@@ -1,66 +1,118 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const {ActionRowBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder} = require('discord.js');
+const {getJson, shuffleArray} = require("../../misc/dataManager");
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('makebingo')
-        .setDescription('Returns an embed of a Bingo.'),
-    async execute(interaction, client) {
-        const embed = new EmbedBuilder().from()
-            .setTitle('Bingo Game')
-            .setDescription('A new bingo game has started! Get your cards ready!')
-            .setColor('#0048ff')
-            .setTimestamp()
+        .setName('bingoboard')
+        .setDescription('Generates a Bingoboard for the given type')
+        .addStringOption(option =>
+            option
+                .setName('type')
+                .setDescription('Type of Sheet that should be generated')
+                .setRequired(true)
+                .setChoices(
+                    {name: 'PC', value: 'pcOnly'},
+                    {name: 'Quest', value: 'questOnly'},
+                    {name: 'Poicast', value: 'poicast'},
+                    {name: 'General', value: 'general'},
+                    {name: 'Gameday', value: 'gamedayOnly'},
+                )),
 
-        const array = [
-            ['string1', 'string2', 'string3', 'string4', 'string5'],
-            ['string6', 'string7', 'string8', 'string9', 'string10'],
-            ['string11', 'string12', 'string13', 'string14', 'string15'],
-            ['string16', 'string17', 'string18', 'string19', 'string20'],
-            ['string21', 'string22', 'string23', 'string24', 'string25']
-        ];
+    async execute(interaction) {
 
-        array.forEach((row, rowIndex) => {
-            row.forEach((string, colIndex) => {
-                embed.addFields(`Cell ${rowIndex + 1}-${colIndex + 1}`, string, true);
-            });
+        const type = interaction.options.getString('type')
+
+        let data = getJson('res/general.json')
+        //TODO optimize
+        let newData = data
+        if (type !== 'general') {
+            const tempData = getJson('res/' + type + '.json')
+            newData = data.concat(tempData)
+        }
+        if (type === 'poicast') {
+            newData = await getJson('res/' + type + '.json')
+        }
+        //JSON.stringify()
+        const shuffledData = shuffleArray(newData);
+        const output = shuffledData.slice(0, 25);
+        const dataArray = Array.from(output).map(obj => JSON.stringify(obj))
+
+
+        for (let i = 0; i < dataArray.length; i++) {
+            console.log(i + " " + typeof dataArray[i] + ":  " + dataArray[i])
+        }
+
+        //oh god lets populate the cells
+
+        const buttons = [];
+        const rows = [];
+
+        for (let i = 0; i < dataArray.length; i++) {
+            const rowNumber = Math.floor(i / 5) + 1;
+            const columnNumber = i % 5 + 1;
+
+            //String.fromCharCode(64 ...) -> letters a-e
+            const button = new ButtonBuilder()
+                .setCustomId(String.fromCharCode(64 + rowNumber) + columnNumber)
+                .setStyle(ButtonStyle.Primary)
+                .setLabel(String.fromCharCode(64 + rowNumber) + columnNumber);
+
+            buttons.push(button);
+
+            if (columnNumber === 1) {
+                const row = new ActionRowBuilder();
+                rows.push(row);
+            }
+
+            rows[rowNumber - 1].addComponents(button);
+        }
+
+        const response = await interaction.reply({
+            content: "imagine a Bingo board here\n" +
+                "yep its hard ik but just imagine for now",
+            components: [rows[0], rows[1], rows[2], rows[3], rows[4]],
         });
 
-       await interaction.channel.send(embed);
-    }
-}
+        /**
+         *
+         *
+         * TODO: This is just a WIP for now, Im too lazy to include that rn.
+         *
+         *
+         const collector = response.createMessageComponentCollector({ componentType: ComponentType.StringSelect, time: 3_600_000 });
 
-// Function to generate a random number between min and max (inclusive)
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+         collector.on('collect', async i => {
+            const selection = i.values[0];
+            await i.reply(`${i.user.username} has selected ${selection}!`);
+        });
 
-// Function to generate a random Bingo field
-function generateBingoField() {
-    const fieldSize = 5;
-    const minNumber = 1;
-    const maxNumber = 75;
-    const field = [];
 
-    // Generate unique random numbers for each cell
-    const numbers = new Set();
-    while (numbers.size < fieldSize * fieldSize - 1) {
-        numbers.add(getRandomNumber(minNumber, maxNumber));
-    }
+         //const collectorFilter = i => i.user.id === interaction.user.id;
+         try {
+            //const confirmation = await response.awaitMessageComponent({ filter: collectorFilter, time: 60_000 })
+            const confirmation = await response.awaitMessageComponent({time: 60_000 })
 
-    // Create the Bingo field array
-    for (let i = 0; i < fieldSize; i++) {
-        const row = [];
-        for (let j = 0; j < fieldSize; j++) {
-            if (i === Math.floor(fieldSize / 2) && j === Math.floor(fieldSize / 2)) {
-                // Add "FREE" space in the center
-                row.push('FREE');
-            } else {
-                // Add a random number
-                row.push(getRandomNumber(minNumber, maxNumber));
+            if (confirmation.customId === 'register') {
+                //await confirmation.update({ content: `${interaction.user.username} has been registered`, components: [] })
+                await ({
+                    ephemeral: true,
+                    content: "You have been registered.",
+                })
+
+                console.log(`${interaction.user.username} has been registered.`)
+            } else if (confirmation.customId === 'cancel') {
+                console.log(`${interaction.user.username} has canceled.`)
+                //await confirmation.update({ content: 'Action cancelled', components: [row2] })
             }
+        } catch (e) {
+            console.log(e)
+            await interaction.reply({
+                content: 'Confirmation not received within 1 minute, cancelling',
+                components: []
+            })
         }
-        field.push(row);
+         */
     }
-
-    return field;
 }
+
+
